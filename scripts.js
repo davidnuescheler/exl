21474cb6fad551432af0df4960f7099835344efd
David Nuescheler <david@nuescheler.ch>
2020-07-10 08:34:57 -0700

    intial desktop layout stuff

--- scripts.js
+++ scripts.js
@@ -8,9 +8,25 @@ function classify(qs, cls, parent) {
 }
 
 function decoratePage() {
-    classify("main>div:first-of-type", "title")
-    classify("main>div h3", "highlighted", 1)
-    classify("main","appear");
+    classify('main>div:first-of-type', 'title')
+    classify('main>div h3', 'highlighted', 1)
+    classify('main','appear');
+    document.querySelectorAll('main>div').forEach(($e) => {
+        if ($e.querySelector('img')) {
+            const $textDiv=document.createElement('div');
+            const $imgDiv=document.createElement('div');
+            $imgDiv.className='image';
+            $textDiv.className='text';
+            Array.from($e.children).forEach(($el) => {
+                if ($el.querySelector('img')) $imgDiv.appendChild($el);
+                else $textDiv.appendChild($el);
+            })
+            $e.innerHTML='';
+            $e.appendChild($imgDiv);
+            $e.appendChild($textDiv);
+            $e.classList.add('text-image');
+        }
+    })
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
